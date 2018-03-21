ebe86c6e6a35b9ee710b5a5e723de7bbc3c373a4
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-21 11:44:41 +0400

    Bootstrap scope dropdown

---
+++ containers/backoffice-front/src/components/scope-dropdown/index.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  b-dropdown(:hoverable="true" position="is-bottom-left")
+    .navbar-item(slot="trigger")
+      span bitfinex/exo
+
+    b-dropdown-item.has-link
+      a(href="/")
+        | exo
+</template>
+
+<script src="./script.js"></script>
