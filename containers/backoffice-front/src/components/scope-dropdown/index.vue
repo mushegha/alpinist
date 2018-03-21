947ee0fd0c5ae15f2e8103ec111eeb7209a32a82
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-21 12:24:50 +0400

    Setup active indicator

--- containers/backoffice-front/src/components/scope-dropdown/index.vue
+++ containers/backoffice-front/src/components/scope-dropdown/index.vue
@@ -1,11 +1,16 @@
 <template lang="pug">
-  b-dropdown(:hoverable="true" position="is-bottom-left")
+  b-dropdown(
+    v-model="activeScope.id"
+    :hoverable="true"
+    position="is-bottom-left"
+  )
     .navbar-item(slot="trigger")
       | {{ activeScope.id }}
 
     b-dropdown-item.has-link(
       v-for="scope in scopes"
       :key="scope.id"
+      :value="scope.id"
     )
       router-link(:to="{ path: 'dashboard', query: scope.query }")
         | {{ scope.id }}
