5182d38a703d0d1c3ad7c0d7218c218c132253b9
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-24 19:16:22 +0400

    Add /ladder route

--- containers/sprint-server/lib/routes/index.js
+++ containers/sprint-server/lib/routes/index.js
@@ -2,12 +2,14 @@ const Router = require('koa-router')
 
 const wallet = require('./wallet')
 const ticker = require('./ticker')
+const ladder = require('./ladder')
 
 function bootstrap () {
   const router = new Router()
 
   router.get('/wallet', wallet())
   router.get('/ticker', ticker())
+  router.get('/ladder', ladder())
 
   return router.routes()
 }
