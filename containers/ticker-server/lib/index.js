358adc9e720ecd351f9ce99510404ebd3b0c5add
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-26 15:01:51 +0400

    Setup Redis dropping RethinkDB

--- containers/ticker-server/lib/index.js
+++ containers/ticker-server/lib/index.js
@@ -4,10 +4,14 @@ const logger = require('koa-logger')
 
 const router = require('./router')
 
+const redis = require('./middlewares/redis')
+
 module.exports = () => {
   const app = new Koa()
 
   app.use(logger())
+
+  app.use(redis())
 
   app.use(router())
 
