e417bfb3a2b91fb822d123d712c9318ffcb5bd4e
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-26 16:11:26 +0400

    Setup ticker router

--- containers/ticker-server/lib/router.js
+++ containers/ticker-server/lib/router.js
@@ -1,19 +1,35 @@
 const Router = require('koa-router')
 
 const {
-  head,
-  assoc
+  evolve,
+  isEmpty
 } = require('ramda')
 
 async function get (ctx) {
-  const { conn, query } = ctx
-  ctx.body = 'exo'
+  const { redis, params } = ctx
+
+  const format = evolve({
+    bid: Number,
+    ask: Number
+  })
+
+  const key = `ticker:${params.symbol}`
+
+  const res = await redis
+    .hgetall(key)
+    .then(format)
+
+  if (isEmpty(res)) {
+    return ctx.throw(404)
+  }
+
+  ctx.body = res
 }
 
 module.exports = () => {
   const router = new Router()
 
-  router.get('/', get)
+  router.get('/:symbol', get)
 
   return router.routes()
 }
