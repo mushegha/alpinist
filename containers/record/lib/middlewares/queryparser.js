31bd7bcfc1ac67a17a27146a63c1a60615c6f641
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-29 17:09:33 +0400

    Implement basic options

--- containers/record/lib/middlewares/queryparser.js
+++ containers/record/lib/middlewares/queryparser.js
@@ -1,10 +1,72 @@
 const {
-  compose
+  unless,
+  compose,
+  reject,
+  isNil,
+  applySpec,
+  cond,
+  prop,
+  equals,
+  always,
+  startsWith,
+  replace
 } = require('ramda')
 
+/**
+ *
+ */
+
+const trader = prop('trader')
+
+const dateClosed = compose(
+  cond([
+    [ equals('open'), always({ $exists: false }) ],
+    [ equals('closed'), always({ $exists: true }) ],
+  ]),
+  prop('status')
+)
+
+const sort = compose(
+  field => {
+    if (!field) return void 0
+
+    const key = replace(/^\-/, '', field)
+    const val = startsWith('-', field) ? -1 : 1
+
+    return { [key]: val }
+  },
+  prop('sort')
+)
+
+const limit = compose(
+  unless(isNil, Number),
+  prop('limit')
+)
+
+/**
+ *
+ */
+
+const parseQuery = compose(
+  reject(isNil),
+  applySpec({
+    trader,
+    dateClosed
+  })
+)
+
+const parseOptions = compose(
+  reject(isNil),
+  applySpec({
+    sort,
+    limit
+  })
+)
 
 module.exports = () => {
   return async function queryparser (ctx, next) {
+    ctx.state.query = parseQuery(ctx.query)
+    ctx.state.options = parseOptions(ctx.query)
 
     return next()
   }
--- containers/record/test/middlewares/queryparser.spec.js
+++ containers/record/test/middlewares/queryparser.spec.js
@@ -2,6 +2,106 @@ import test from 'ava'
 
 import queryparser from '../../lib/middlewares/queryparser'
 
-test('initial', async t => {
-  t.is(typeof queryparser(), 'function', 'returns middleware')
+/**
+ * Helpers
+ */
+
+const next = () => Promise.resolve()
+
+const fn = queryparser()
+
+
+test('?trader=xxx', async t => {
+  const state = {}
+  const query = {
+    trader: 'xxx'
+  }
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(
+    state.query,
+    { trader: 'xxx' }
+  )
+})
+
+test('?status=open', async t => {
+  const state = {}
+  const query = {
+    status: 'open'
+  }
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(
+    state.query,
+    { dateClosed: { $exists: false } }
+  )
+})
+
+test('?status=closed', async t => {
+  const state = {}
+  const query = {
+    status: 'closed'
+  }
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(
+    state.query,
+    { dateClosed: { $exists: true } }
+  )
+})
+
+test('?sort=priceInitial', async t => {
+  const state = {}
+  const query = {
+    sort: 'priceInitial'
+  }
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(
+    state.options,
+    { sort: { priceInitial: 1 } }
+  )
+})
+
+test('?sort=-dateOpened', async t => {
+  const state = {}
+  const query = {
+    sort: '-dateOpened'
+  }
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(
+    state.options,
+    { sort: { dateOpened: -1 } }
+  )
+})
+
+test('?limit=4', async t => {
+  const state = {}
+  const query = {
+    limit: '4'
+  }
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(
+    state.options,
+    { limit: 4 }
+  )
+})
+
+test('?', async t => {
+
+  const state = {}
+  const query = {}
+
+  await fn({ state, query }, next)
+
+  t.deepEqual(state.query, {})
+  t.deepEqual(state.options, {})
 })
