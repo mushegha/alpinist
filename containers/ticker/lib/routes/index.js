05eaa24af84c91e56de566083409c8a0f0cfa2be
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-03-30 13:59:16 +0400

    Add route to get all tickers

--- containers/ticker/lib/routes/index.js
+++ containers/ticker/lib/routes/index.js
@@ -1,8 +1,22 @@
 const Router = require('koa-router')
 
+const { map } = require('ramda')
+
+const getenv = require('getenv')
+
 const getOne = require('../getters/redis-one')
 
-async function get (ctx) {
+const symbols = getenv.array('TICKER_SYMBOLS', 'string', [])
+
+async function list (ctx) {
+  const { redis } = ctx
+
+  ctx.body = await Promise.all(
+    map(getOne(redis), symbols)
+  )
+}
+
+async function read (ctx) {
   const { redis, params } = ctx
 
   const res = await getOne(redis, params.symbol)
@@ -15,7 +29,8 @@ async function get (ctx) {
 module.exports = () => {
   const router = new Router()
 
-  router.get('/:symbol', get)
+  router.get('/', list)
+  router.get('/:symbol', read)
 
   return router.routes()
 }
