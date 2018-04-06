64b893b4ddd7ab6e989f247a813adacc64b3c8ce
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-04-06 05:14:23 +0400

    Place liquidate/destroy far

--- containers/dashboard/src/components/trader-panel/index.vue
+++ containers/dashboard/src/components/trader-panel/index.vue
@@ -1,32 +1,32 @@
 <template lang="pug">
-.panel
-  div.panel-block
-    .field.is-horizontal
-      .field-label
-        | {{ value.isRunning ? 'Pause' : 'Start' }}
-      .field-body
-        .control
-          b-switch(
-            v-model="value.isRunning"
-            @input="toggleStatus"
-          )
-  a.panel-block(:href="'/' + id + '/edit'")
-    | Edit configuration
-  a.panel-block(@click.prevent="downloadCSV")
-    | Download spreadsheet
+div
+  .panel
+    div.panel-block
+      .field.is-horizontal
+        .field-label
+          | {{ value.isRunning ? 'Pause' : 'Start' }}
+        .field-body
+          .control
+            b-switch(
+              v-model="value.isRunning"
+              @input="toggleStatus"
+            )
+    a.panel-block(:href="'/' + id + '/edit'")
+      | Edit configuration
+    a.panel-block(@click.prevent="downloadCSV")
+      | Download spreadsheet
 
-  .panel-block.has-text-danger
-    h4 Danger Zone
-
-  .panel-block
-    .control
-      a.button.is-warning(@click.prevent="liquidateAll")
-        | Liquidate
-
-  .panel-block
-    .control
-      a.button.is-danger.is-outlined(@click.prevent="destroyTrader")
-        | Destroy instance
+  b-collapse.panel(:open="false")
+    .panel-heading(slot="trigger")
+      strong Manage
+    .panel-block
+      .control
+        a.button.is-warning(@click.prevent="liquidateAll")
+          | Liquidate
+    .panel-block
+      .control
+        a.button.is-danger.is-outlined(@click.prevent="destroyTrader")
+          | Destroy instance
 </template>
 
 <script src="./script.js"></script>
