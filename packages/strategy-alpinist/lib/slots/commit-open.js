c5284534a61e5db890f82daa074ec23a8baf2c50
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-04-27 08:45:59 +0400

    Fix commit-open

--- packages/strategy-alpinist/lib/slots/commit-open.js
+++ packages/strategy-alpinist/lib/slots/commit-open.js
@@ -1,65 +1,104 @@
 const {
+  add,
+  multiply,
+  append,
   sortBy,
   prop,
   isEmpty,
   compose,
   last,
-  head
+  head,
+  curryN
 } = require('ramda')
 
 const { Maybe } = require('monet')
 
-const maxLevel = compose(prop('level'), last)
-const maxWeight = compose(prop('weight'), last)
+const ybkx = curryN(3, (b, k, x) => b + k * x)
 
-const minLevel = compose(prop('level'), head)
-const minWeight = compose(prop('weight'), head)
+const sorted = sortBy(prop('level'))
 
-function renderWeight (opts, level, slots = []) {
-  const {
-    level_threshold,
-    weight_initial,
-    weight_up_b,
-    weight_up_k,
-    weight_down_b,
-    weight_down_k
-  } = opts
+const header = compose(last, sorted)
+const footer = compose(head, sorted)
 
-  if (isEmpty(slots)) {
-    return Maybe.some(weight_initial)
-  }
+const headerLevel = compose(prop('level'), header)
+const footerLevel = compose(prop('level'), footer)
 
-  if (level <= minLevel(slots) - level_threshold) {
-    const weight =
-    return Maybe.some
-  }
-}
+const headerWeight = compose(prop('weight'), header)
+const footerWeight = compose(prop('weight'), footer)
+
+const investmentFrom = ({ level, weight }) =>
+  level * weight
+
+const headerInvestment = compose(investmentFrom, header)
+const footerInvestment = compose(investmentFrom, footer)
 
 /**
  * Append new slot on given `level` if available
  *
  * @param {Object} opts
  * @param {number} opts.level_threshold
- * @param {number} opts.weight_initial -
- * @param {number} opts.weight_up_b - Addition
- * @param {number} opts.weight_up_k - Multiplication
- * @param {number} opts.weight_down_b
- * @param {number} opts.weight_down_k
+ * @param {number} opts.investment_initial -
+ * @param {number} opts.investment_up_b - Addition
+ * @param {number} opts.investment_up_k - Multiplication
+ * @param {number} opts.investment_down_b
+ * @param {number} opts.investment_down_k
  * @param {number} level
  * @param {Array}  slots
  *
- * @returm {Array} - With new slots appended
+ * @returm {Maybe} - Next weight
  */
 
-function commitOpen (opts, level, slots = []) {
-  const sortedSlots = sortBy(prop('level'), slots)
+function renderWeight (opts, level, slots = []) {
+  const {
+    level_threshold,
+    investment_initial,
+    investment_up_b,
+    investment_up_k,
+    investment_down_b,
+    investment_down_k
+  } = opts
 
   if (isEmpty(slots)) {
-    return {
-
-    }
+    const weight = investment_initial / level
+    return Maybe.Some(weight)
   }
 
+  const toWeight = investment => investment / level
+
+  const weightNextHeader = compose(
+    toWeight,
+    add(investment_up_b),
+    multiply(investment_up_k),
+    headerInvestment
+  )
+
+  const weightNextFooter = compose(
+    toWeight,
+    add(investment_down_b),
+    multiply(investment_down_k),
+    footerInvestment
+  )
+
+  const marginHeader = headerLevel(slots) + level_threshold
+  const marginFooter = footerLevel(slots) - level_threshold
+
+  if (level >= marginHeader) {
+    return Maybe.Some(weightNextHeader(slots))
+  }
+
+  if (level <= marginFooter) {
+    return Maybe.Some(weightNextFooter(slots))
+  }
+
+  return Maybe.None()
 }
 
-module.exports = commitOpen
+function commitOpen (opts, level, slots = []) {
+  return renderWeight(opts, level, slots)
+    .map(weight => ({ level, weight }))
+    .map(slot => append(slot, slots))
+    .map(sorted)
+    .orSome(slots)
+}
+
+module.exports = curryN(3, commitOpen)
--- packages/strategy-alpinist/test/commit-open.spec.js
+++ packages/strategy-alpinist/test/commit-open.spec.js
@@ -2,6 +2,54 @@ import test from 'ava'
 
 import { commitOpen } from '../lib/slots'
 
-test('commitOpen', t => {
-  t.is(typeof commitOpen, 'function')
+const commit = commitOpen({
+  level_threshold: 5,
+  investment_initial: 100,
+  investment_up_b: 10,
+  investment_up_k: 2,
+  investment_down_b: 15,
+  investment_down_k: 1.75
+})
+
+test('empty', t => {
+  const level = 100
+
+  const oldSlots = []
+
+  const newSlots = commit(level, oldSlots)
+
+  t.deepEqual(newSlots, [
+    { level: 100, weight: 1 }
+  ])
+})
+
+test('some', t => {
+  const oldSlots = [
+    { level: 100, weight: 1 }
+  ]
+
+  const newSlotsUp = commit(105, oldSlots)
+  const newSlotsDown = commit(95, oldSlots)
+
+  t.deepEqual(newSlotsUp, [
+    { level: 100, weight: 1 },
+    { level: 105, weight: 2 }
+  ])
+
+  t.deepEqual(newSlotsDown, [
+    { level: 95, weight: 2 },
+    { level: 100, weight: 1 }
+  ])
+})
+
+test('none', t => {
+  const oldSlots = [
+    { level: 100, weight: 1 }
+  ]
+
+  const newSlotsUp = commit(104, oldSlots)
+
+  t.deepEqual(newSlotsUp, [
+    { level: 100, weight: 1 }
+  ])
 })
