5c7a9959f52211f05d90e95279ec8cc829517078
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-05-10 20:47:31 +0400

    Update route create with queue

--- containers/order-server/lib/koa-routes/create.js
+++ containers/order-server/lib/koa-routes/create.js
@@ -1,53 +1,10 @@
-const R = require('ramda')
-
-const { ulid } = require('ulid')
-
-const SOURCE_FIELDS = [
-  'subject',
-  'operation',
-  'kind',
-  'exchange',
-  'symbol',
-  'quantity',
-  'price'
-]
-
-const bodyLens = R.lensPath(['request', 'body'])
-
-const subjectLens = R.lensProp('subject')
-const statusLens = R.lensProp('status')
-
-const fromContext = R.compose(
-  R.over(subjectLens, ulid),
-  R.over(statusLens, R.always('new')),
-  R.pick(SOURCE_FIELDS),
-  R.view(bodyLens)
-)
-
-const asBuffer = R.compose(
-  Buffer.from,
-  JSON.stringify
-)
+const { addOrderTo } = require('@alpinist/order-queue/lib/fn')
 
 function create () {
   return async (ctx, next) => {
-    ctx.body = fromContext(ctx)
+    const { queue, request } = ctx
 
-    ctx.mqtt.publish('orders', asBuffer(ctx.body))
-
-    await new Promise(resolve => {
-      ctx.mqtt.on('message', (topic, buffer) => {
-        const data = JSON.parse(buffer.toString())
-
-        if (data.subject === ctx.body.subject) {
-          if (data.status === 'created') {
-            console.log(data)
-            resolve(data)
-          }
-        }
-
-      })
-    })
+    ctx.body = await addOrderTo(queue, request.body)
 
     return next()
   }
--- containers/order-server/server.js
+++ containers/order-server/server.js
@@ -3,9 +3,13 @@ const Koa = require('koa')
 const logger = require('koa-logger')
 const bodyParser = require('koa-bodyparser')
 
+const Queue = require('@alpinist/order-queue')
+
 const routes = require('./lib/koa-routes')
 
 const app = new Koa()
+
+app.context.queue = new Queue()
 
 app.use(logger())
 app.use(bodyParser())
