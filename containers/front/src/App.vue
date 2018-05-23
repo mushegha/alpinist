57523e202f17cd7d2601290f5ec94c2bf2902b03
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-05-23 18:08:36 +0400

    Install ElementUI

--- containers/front/src/App.vue
+++ containers/front/src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <el-container id="app">
+    <el-header>Header</el-header>
+    <el-main>
+      <img src="./assets/logo.png">
+      <router-view/>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <script>
@@ -11,13 +15,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style> </style>
