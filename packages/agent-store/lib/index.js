974bc99ab3dfe8efbec8202b24ded0621139f0ef
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-05-27 02:43:07 +0400

    Use plugins for agent store

--- packages/agent-store/lib/index.js
+++ packages/agent-store/lib/index.js
@@ -1,1 +1,22 @@
-module.exports = require('./store')
+const PouchDB = require('pouchdb')
+
+const { merge } = require('ramda')
+
+const upsertPlugin = require('pouchdb-upsert')
+const findPlugin = require('pouchdb-find')
+
+const connectPlugin = require('@alpinist/pouchdb-connect')
+
+const agentMethods = require('@alpinist/pouchdb-agent-methods')
+
+PouchDB
+  .plugin(upsertPlugin)
+  .plugin(findPlugin)
+  .plugin(connectPlugin)
+  .plugin(agentMethods)
+
+function Store () {
+  return new PouchDB('http://localhost:5984/agents')
+}
+
+module.exports = Store
