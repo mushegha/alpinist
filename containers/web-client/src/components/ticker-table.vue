e335bbb5aac48028249b3ac08ff3f66feae41efd
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-05-27 12:54:42 +0400

    Refine ticker table

--- containers/web-client/src/components/ticker-table.vue
+++ containers/web-client/src/components/ticker-table.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
   el-table(
     v-if="dataset"
-    :data="dataset"
-    stripe)
+    :data="dataset")
 
     el-table-column(type="expand")
       template(slot-scope="props")
@@ -12,33 +11,115 @@
     el-table-column(
       label="Broker"
       prop="broker"
-      sortable)
+      :filters="filtersFor('broker')"
+      :filter-method="filterHandler")
 
     el-table-column(
       label="Symbol"
-      prop="symbol")
+      prop="symbol"
+      :filters="filtersFor('symbol')"
+      :filter-method="filterHandler")
 
     el-table-column(
-      label="Bid Price"
-      prop="ticker.bid_price")
+      label="Bid"
+      align="center")
+
+      el-table-column(
+        label="Price"
+        align="center"
+        prop="bidPrice"
+        :sortable="true")
+
+      el-table-column(
+        label="Volume"
+        align="center"
+        prop="bidVolume")
 
     el-table-column(
-      label="Ask Price"
-      prop="ticker.ask_price")
+      label="Ask"
+      align="center")
+
+      el-table-column(
+        label="Price"
+        align="center"
+        prop="askPrice"
+        :sortable="true")
+
+      el-table-column(
+        label="Volume"
+        align="center"
+        prop="askVolume")
+
+    el-table-column(
+      label="Updated"
+      align="right"
+      prop="time"
+      :formatter="dateFormatter")
 </template>
 
-<script>
+<style>
+.is-best {
+  background: #ddffdd;
+}
+</style>
 
+<script>
 import { mapGetters } from 'vuex'
 
+import {
+  distanceInWords,
+  format as formatDate
+} from 'date-fns'
+
+import {
+  map,
+  prop,
+  compose,
+  applySpec,
+  uniq,
+  identity
+} from 'ramda'
+
 const computed = {
+  filtersFor () {
+    const fromValue = applySpec({
+      text: identity,
+      value: identity
+    })
+
+    return key => {
+      const compile = compose(
+        map(fromValue),
+        uniq,
+        map(prop(key))
+      )
+
+      return compile(this.dataset)
+    }
+  },
   ...mapGetters({
     dataset: 'tickers/asArray'
   })
 }
 
+const methods = {
+  dateFormatter (row, col) {
+    const time = row.time
+    const now = Date.now()
+
+    return now - time > 5 * 60 * 1e3
+      ? formatDate(time, 'HH:mm:ss')
+      : distanceInWords(time, now, { includeSeconds: true })
+  },
+  filterHandler (val, row, col) {
+    const { property } = col
+    return row[property] === val
+  }
+}
+
 export default {
   name: 'ticker-table',
-  computed
+  computed,
+  methods
 }
 </script>
