bf37f74eb573483f4e010a26afb38af8124ec8ac
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-06-01 23:07:22 +0400

    Setup mqtt publishing

--- containers/ticker-server/producer.js
+++ containers/ticker-server/producer.js
@@ -1,4 +1,15 @@
+const mqtt = require('mqtt')
+
 const Monitor = require('./lib/monitor')
 
-Monitor()
-  .subscribe(console.log)
+const client = mqtt.connect('mqtt://178.62.246.62:1883')
+
+const publish = tick => {
+  const topic = `tickers/${tick.broker}`
+  const message = JSON.stringify(tick)
+
+  return client.publish(topic, message)
+}
+
+client.on('error', console.error)
+client.on('connect', _ => Monitor().subscribe(publish))
