ca57b7773584f08c7ff1f705a0d293b787dbd3cf
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2018-06-15 20:32:08 +0400

    Sync orders w/ remote in agent server

---
+++ containers/agent-server/connect.js
@@ -0,0 +1,113 @@
+const debug = require('debug')('alpinist:orders:sync')
+
+const {
+  assoc,
+  both,
+  either,
+  identity,
+  tap,
+  dissoc,
+  propEq,
+  complement
+} = require('ramda')
+
+const {
+  OrdersObservable,
+  OrdersObserver
+} = require('./lib/channel')
+
+const { Orders } = require('./lib/store')
+
+/**
+ * Helpers
+ */
+
+const isBuyOrder = propEq('side', 'buy')
+
+const isSellOrder = propEq('side', 'sell')
+
+const isNewBuy = both(
+  isBuyOrder,
+  propEq('buy_status', 'new')
+)
+
+const isNewSell = both(
+  isSellOrder,
+  propEq('sell_status', 'new')
+)
+
+const isNew = either(isNewBuy, isNewSell)
+
+const isClosed = propEq('status', 'closed')
+
+const orderFrom = slot => {
+  const price = isBuyOrder(slot)
+    ? slot.buy_price
+    : slot.sell_price
+
+  return assoc('price', price, slot)
+}
+
+/**
+ *
+ */
+
+const source = new OrdersObservable()
+
+const sink = new OrdersObserver()
+
+const store = new Orders()
+
+/**
+ *
+ */
+
+source
+  .filter(isClosed)
+  .subscribe(order => {
+    debug('Order details: %O', order)
+
+    const {
+      id,
+      side,
+      price,
+      status
+    } = order
+
+    const slotProps = {
+      id,
+      side
+    }
+
+    if (side === 'buy') {
+      slotProps.buy_price = price
+      slotProps.buy_status = status
+    } else {
+      slotProps.sell_price = price
+      slotProps.sell_status = status
+    }
+
+    store
+      .putOrder(slotProps)
+      .then(_ => debug('Updated %s to %s %s', id, side, status))
+  })
+
+/**
+ *
+ */
+
+store
+  .source()
+  .filter(isNew)
+  .map(orderFrom)
+  .subscribe(sink)
+
+/**
+ *
+ */
+
+store
+  .source()
+  .subscribe(order => {
+    debug('Order snapshot received: %s %s', order.side, order.status)
+  })
